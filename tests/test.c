48e1339f074ee7511ab5348b958be641fa5ac5ec
devdepijama <hugo@devdepijama.com.br>
2020-12-11 00:33:00 +0000

    Added a test for logger

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+#include "assertions/assertions.h"
+#include "log/logger.h"
+
+void test_logger() {
+    logger_t logger;
+
+    printf("--- > Testing logger \n");
+
+    assert_equals(LOGGER_E_SUCCESSFUL, logger_create(&logger, "myLogger"), "Failed to construct logger");
+    logger_info(logger, "Just a regular message on INFO level with argument %d", 2);
+    logger_warn(logger, "Just a regular message on WARN level");
+    logger_error(logger, "Just a regular message on ERROR level");
+
+    logger_destroy(logger);
+}
+
 int main() {
-    printf("Running tests");
+    printf("Running tests \n");
+
+    test_logger();
+
+    printf("Finished tests Successfully :)");
     return 0;
 }
