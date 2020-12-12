3c1ba4f2dcaac7debf443c9a939f931f6130f896
devdepijama <hugo@devdepijama.com.br>
2020-12-12 00:34:10 +0000

    Improving code quality

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,32 +1,25 @@
-#include "memory.h"
+#include "memory/memory.h"
 #include "log/logger.h"
-#include "settings/constants.h"
 
 #include <stdlib.h>
 
 static logger_t logger;
 
 void memory_init(memory_parameters_s *parameters) {
-    memory_parameters_s default_parameters = {
-        .log_level = CONSTANT_LOG_LEVEL,
-        .logName = "memory"
-    };
-
-    parameters = (!parameters) ? &default_parameters : parameters;
-    logger_create(&logger, parameters->logName, parameters->log_level);
+    logger = parameters->logger;
 }
 
 void * memory_alloc(unsigned int size, char *description) {
     void *result = malloc(size);
     if (!result) {
-        logger_error(logger, "[addr:0x%X][size:%d bytes] Failed to allocate memory for %s. Out of memory, bro? :P", result, size, description);
+        logger_error(logger, "[addr:0x%X][bytes:%d] Failed to allocate memory for %s. Out of memory, bro? :P", result, size, description);
         exit(-1);
     }
 
-    logger_debug(logger, "[addr:0x%X][size:%d bytes] Allocated memory successfully for %s", result, size,  description);
+    logger_debug(logger, "[addr:0x%X][bytes:%d] Allocated memory successfully for %s", result, size,  description);
     return result;
 }
-void memory_free(void *ptr) {
-    logger_debug(logger, "[addr:0x%X] Freeing allocated memory...", ptr);
+void memory_free(void *ptr, char *description) {
+    logger_debug(logger, "[addr:0x%X] Freeing memory for %s", ptr, description);
     return free(ptr);
 }
--- src/memory/memory.h
+++ src/memory/memory.h
@@ -1,13 +1,14 @@
 #ifndef H_MEMORY
 #define H_MEMORY
 
+#include "log/logger.h"
+
 typedef struct {
-    char *logName;
-    int log_level;
+    logger_t logger;
 } memory_parameters_s;
 
 void memory_init(memory_parameters_s *parameters);
 void * memory_alloc(unsigned int size, char *description);
-void memory_free(void *ptr);
+void memory_free(void *ptr, char *description);
 
 #endif
